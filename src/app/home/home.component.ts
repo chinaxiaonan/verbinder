48f1a0b3d54d590b9dc9a40de239862da88685b2
z003makd <xiaonan.liu@siemens.com>
2020-01-07 21:16:09 +0800

    new modify;init home page

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,5 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit  } from '@angular/core';
 import { Router } from '@angular/router';
+import { UploadFile, UploadFilter } from 'ng-zorro-antd/upload';
+import { Observable, Observer } from 'rxjs';
+import { NzMessageService } from 'ng-zorro-antd/message';
 
 @Component({
   selector: 'app-home',
@@ -7,13 +10,67 @@ import { Router } from '@angular/router';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
-
-  constructor(private router: Router){}
+  requirement: string;
+  reqPlan:string;
+  expection:string;
+  expectDelivery:string;
+  public nextAvi: boolean;
+  constructor(private msg: NzMessageService,private router: Router){}
   
   redirect(){
     this.router.navigate(["process"]);
   }
   ngOnInit() {
+    this.nextAvi = true;
   }
 
+  filters: UploadFilter[] = [
+    {
+      name: 'type',
+      fn: (fileList: UploadFile[]) => {
+        const filterFiles = fileList.filter(w => ~['image/png'].indexOf(w.type));
+        if (filterFiles.length !== fileList.length) {
+          this.msg.error(`包含文件格式不正确，只支持 png 格式`);
+          return filterFiles;
+        }
+        return fileList;
+      }
+    },
+    {
+      name: 'async',
+      fn: (fileList: UploadFile[]) => {
+        return new Observable((observer: Observer<UploadFile[]>) => {
+          // doing
+          observer.next(fileList);
+          observer.complete();
+        });
+      }
+    }
+  ];
+
+  fileList = [
+    {
+      uid: -1,
+      name: 'xxx.png',
+      status: 'done',
+      url: 'http://www.baidu.com/xxx.png'
+    }
+  ];
+
+  // tslint:disable-next-line:no-any
+  handleChange(info: any): void {
+    const fileList = info.fileList;
+    // 2. read from response and show file link
+    if (info.file.response) {
+      info.file.url = info.file.response.url;
+    }
+    // 3. filter successfully uploaded files according to response from server
+    // tslint:disable-next-line:no-any
+    this.fileList = fileList.filter((item: any) => {
+      if (item.response) {
+        return item.response.status === 'success';
+      }
+      return true;
+    });
+  }
 }
